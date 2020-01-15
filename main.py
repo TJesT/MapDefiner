7f7a3d7a2772256409ac8d8071a09dd0170cc947
TJesT <alexeynospid@gmail.com>
2020-01-16 01:03:04 +0300

    adaptive rotation

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from controlBars import thresh_bars_hsv as tbh, erode_dilate_bars as edb
 import numpy as np
 import cv2
 
+def rotate_image(image, angle):
+    h, w = image.shape[:2]
+    cX, cY = w//2, h//2
+ 
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
+ 
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
+ 
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
+ 
+    return cv2.warpAffine(image, M, (nW, nH))
+
 def get_kernel(id_int):
     ret = []
     with open("kernels.txt") as op:
@@ -42,8 +58,22 @@ for fn in hsv_data.keys():
 #edb("1", thresh)
     morph = cv2.erode(thresh, kernel=erode_kernel,  iterations=1)
     morph = cv2.dilate(morph, kernel=dilate_kernel, iterations=1)
-
+    
     morph = cv2.resize(morph, (640, 640))
+    #img = cv2.resize(data[fn], (640, 640))
+    
+    lines = cv2.HoughLinesP(morph, 1, np.pi / 180, 50, 300, 100, 0)
+    
+    h_line = sorted(lines, key=lambda line: max(line.ravel()[1], line.ravel()[3]))[-1].ravel()
+    phi = np.arctan2(abs(h_line[3] - h_line[1]),abs(h_line[2] - h_line[0]))*180/np.pi
+    print(phi)
+    
+    morph = rotate_image(morph, phi)
+    
+#    cv2.line(img,(h_line[0], h_line[1]),(h_line[2],h_line[3]),(0,255,0),2)
+#    for line in lines:
+#        for x1,y1,x2,y2 in line:
+#            cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
     cv2.imshow(fn + " thresholded", morph)
 
