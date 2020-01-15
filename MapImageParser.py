51cff43a84d33e24476e3297e6e1da201b790d3d
TJesT <alexeynospid@gmail.com>
2020-01-15 14:03:14 +0300

    main + class setup

---
+++ MapImageParser.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+
+class MapDefiner:
+    def __init__(self, image):
+        self.__image = image
+        self.__map  = dict()
+        return
+    
+    def define(self):
+        #do smth with __image
+        pass
+    
+    def get_map(self):
+        return self.__map
---
+++ main.py
@@ -0,0 +1,12 @@
+from os.path import isfile, join
+from os import listdir
+import MapImageParser as MIP
+
+maps = "maps/"
+list()
+
+#data = {
+#    fn: cv2.imread(join(maps, fn)) for fn in listdir(maps) if isfile(fn)
+#}
+
+ mip = MIP()
